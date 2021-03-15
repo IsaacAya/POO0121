2304258348d8328f2b5b0859b04c08df0da4cdfd
IsaacAya <00274119@uca.edu.sv>
2021-03-14 21:35:04 -0600

    Cambiando de HW a Programa de sumas

--- OptativaGH/Program.cs
+++ OptativaGH/Program.cs
@@ -5,8 +5,14 @@ namespace OptativaGH
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   var a = 0;
+            var b = 0;
+            var c = 0;
+            Console.WriteLine("Ingrese dos numeros:");
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine($"La suma de los numeros es: {c}.");
         }
     }
 }
