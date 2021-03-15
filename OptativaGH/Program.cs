9f9eadf9e6e4fe85f85cdca751e1ae254616aadc
DaniloBonillaUCA <danilobonilla04@gmail.com>
2021-03-14 23:27:43 -0600

    Modify program

--- OptativaGH/Program.cs
+++ OptativaGH/Program.cs
@@ -5,14 +5,27 @@ namespace OptativaGH
     class Program
     {
         static void Main(string[] args)
-        {   var a = 0;
+        {   //The user is welcomed
+            Console.WriteLine("Este es un programa de prueba");
+
+            //Declaring implicit variables
+            var a = 0;
             var b = 0;
             var c = 0;
+            
+            //Instructions for the user
             Console.WriteLine("Ingrese dos numeros:");
+            
+            //Converting text string to integer
             a = Int32.Parse(Console.ReadLine());
             b = Int32.Parse(Console.ReadLine());
+
+            //The sum of the numbers is performed
             c = a + b;
+            
+            //The result is displayed
             Console.WriteLine($"La suma de los numeros es: {c}.");
+            Console.WriteLine("La suma se ha realizado correctamente. Tenga un feliz dia");
         }
     }
 }
